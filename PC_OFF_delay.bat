b7594c2133235a6761712b6694f37f737272c42d
Ptr <krolak.piotr.lca@gmail.com>
2018-11-03 00:35:03 +0100

    dodanie opóźnienia w minutach ~ Ptr

--- PC_OFF_delay.bat
+++ PC_OFF_delay.bat
@@ -1,1 +1,12 @@
-shutdown /s /t 60 
+@echo off
+title Wylacz PC
+
+echo Podaj czas do wylaczenia PC [min]
+set /p czas=czas:
+set /A czas = %czas% * 10 
+
+shutdown /s /t %czas% 
+
+echo Wylaczenie PC nastapi za:
+timeout %czas%
+pause 
