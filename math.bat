83a4f6102fdb54f4be99703d32f6c7df21c20683
Ptr <krolak.piotr.lca@gmail.com>
2018-11-03 00:14:17 +0100

    trening .bat - zmienne + math ~ Ptr

---
+++ math.bat
@@ -0,0 +1,16 @@
+@echo off
+
+title math.bat
+echo "=================================" 
+echo "============DODAWANIE============"
+echo "================================="
+
+echo "Podaj pierwsza liczbe"
+set /p liczba_1 = liczba_1:
+
+echo "Podaj druga liczbe"
+set /p liczba_2 = liczba_2:
+
+echo "%liczba_1% %liczba_2%"
+
+pause
